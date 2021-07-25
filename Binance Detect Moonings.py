5f8a16baf57783d5416ab373e67ba7608ff874ba
dragoncity17 <pierreall1@gmail.com>
2021-07-25 18:53:48 +0200

    Color Blue for Loss and Green for Profit

--- Binance Detect Moonings.py
+++ Binance Detect Moonings.py
@@ -77,7 +77,7 @@ from helpers.handle_creds import (
 class txcolors:
     BUY = '\033[92m'
     WARNING = '\033[93m'
-    SELL_LOSS = '\033[91m'
+    SELL_LOSS = '\033[94m'
     SELL_PROFIT = '\033[32m'
     DIM = '\033[2m\033[35m'
     DEFAULT = '\033[39m'
