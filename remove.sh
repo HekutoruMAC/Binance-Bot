153a123d696df6e728e88614675bf7367f188388
Pantersxx3 <horacio.fanelli@gmail.com>
2021-08-09 12:43:48 -0300

    Add files via upload

---
+++ remove.sh
@@ -0,0 +1,30 @@
+@echo off
+
+echo Eliminado live_bot_stats.json...
+rm live_bot_stats.json 2>/dev/null
+echo 
+echo Eliminado live_coins_bought
+rm live_coins_bought.json 2>/dev/null
+echo 
+echo Eliminado live_history
+rm live_history.txt 2>/dev/null
+echo 
+echo Eliminado live_trades
+rm live_trades.txt 2>/dev/null
+echo 
+echo Eliminado signalsell_tickers
+rm signalsell_tickers.txt 2>/dev/null
+echo 
+echo Eliminado test_bot_stats
+rm test_bot_stats.json 2>/dev/null
+echo 
+echo Eliminado test_coins_bought
+rm test_coins_bought.json 2>/dev/null
+echo 
+echo Eliminado est_history
+rm test_history.txt 2>/dev/null
+echo 
+echo Eliminado test_trades
+rm test_trades.txt 2>/dev/null
+echo 
+echo Se han eliminado los archivos..
