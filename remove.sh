cf1945c5d1eac66c02b1ca42e1555a25ac8bcffb
Pantersxx3 <horacio.fanelli@gmail.com>
2021-09-30 16:16:09 -0300

    upgrade
    It was updated based on the new improvements of the bot

--- remove.sh
+++ remove.sh
@@ -7,10 +7,10 @@ echo Eliminado live_coins_bought
 rm live_coins_bought.json 2>/dev/null
 echo 
 echo Eliminado live_history
-rm live_history.txt 2>/dev/null
+rm live_history.html 2>/dev/null
 echo 
 echo Eliminado live_trades
-rm live_trades.txt 2>/dev/null
+rm live_trades.html 2>/dev/null
 echo 
 echo Eliminado signalsell_tickers
 rm signalsell_tickers.txt 2>/dev/null
@@ -22,9 +22,9 @@ echo Eliminado test_coins_bought
 rm test_coins_bought.json 2>/dev/null
 echo 
 echo Eliminado est_history
-rm test_history.txt 2>/dev/null
+rm test_history.html 2>/dev/null
 echo 
 echo Eliminado test_trades
-rm test_trades.txt 2>/dev/null
+rm test_trades.html 2>/dev/null
 echo 
 echo Se han eliminado los archivos..
