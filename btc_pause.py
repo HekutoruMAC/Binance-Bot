0aad481f1332e8b84bf42d58611b1253f8bc4093
Pantersxx3 <horacio.fanelli@gmail.com>
2022-04-25 19:27:58 -0300

    update
    optimized

--- btc_pause.py
+++ btc_pause.py
@@ -53,32 +53,15 @@ parsed_config = load_config(config_file)
 
 # Load trading vars
 PAIR_WITH = parsed_config['trading_options']['PAIR_WITH']
-TEST_MODE = parsed_config['script_options']['TEST_MODE']
-TAKE_PROFIT = parsed_config['trading_options']['TAKE_PROFIT']
 DISCORD_WEBHOOK = load_discord_creds(parsed_creds)
 
 # Load creds for correct environment
 access_key, secret_key = load_correct_creds(parsed_creds)
 client = Client(access_key, secret_key)
 
-
-CREATE_TICKER_LIST = True
-ticker_type = 'all'
-if CREATE_TICKER_LIST:
-	TICKERS_LIST = 'tickers_all_USDT.txt'
-else:
-	TICKERS_LIST = 'tickers.txt'
-
-# System Settings
-# BVT = False
-# OLORIN = True  # if not using Olorin Sledge Fork set to False
-# if OLORIN:
-	# signal_file_type = '.buy'
-# else:
-    #signal_file_type = '.exs'
-
-# if using Windows OS set to True, else set to False
-#WINDOWS = True
+TIME_TO_WAIT = 1
+FULL_LOG = False
+DEBUG = True
 
 # send message to discord
 DISCORD = False
@@ -91,26 +74,17 @@ LIMIT = 6
 INTERVAL = '1m'
 percent = 1
 
-websocket_server_url = 'https://api.binance.com/api/v1/'
-timestamp_log = datetime.now().strftime("%y-%m-%d %H:%M:%S")
 SIGNAL_NAME = 'btc_pause'
 SIGNAL_TYPE = 'pause'
 
-class TextColors:
-	BUY = '\033[92m'
-	WARNING = '\033[93m'
-	SELL_LOSS = '\033[91m'
-	SELL_PROFIT = '\033[32m'
-	DIM = '\033[2m\033[35m'
-	DEFAULT = '\033[39m'
-	YELLOW = '\033[33m'
-	TURQUOISE = '\033[36m'
-	UNDERLINE = '\033[4m'
-	END = '\033[0m'
-	ITALICS = '\033[3m'
-	TCR = '\033[91m'
-	TCG = '\033[32m'
-	TCD = '\033[39m'
+class txcolors:
+    BUY = '\033[92m'
+    WARNING = '\033[93m'
+    SELL_LOSS = '\033[91m'
+    SELL_PROFIT = '\033[32m'
+    DIM = '\033[2m\033[35m'
+    Red = "\033[31m"
+    DEFAULT = '\033[39m'
 
 def msg_discord(msg):
 	message = msg + '\n\n'
@@ -120,71 +94,75 @@ def msg_discord(msg):
 
 def analyse_btc():
     global paused
-	# Normal Scan for LIMIT and INTERVAL
-    exchange = ccxt.binance()
-    try:
-        btc = exchange.fetch_ohlcv("BTC" + PAIR_WITH, timeframe='1m', limit=25)
-        btc = pd.DataFrame(btc, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
-        btc['VWAP'] = ((((btc.high + btc.low + btc.close) / 3) * btc.volume) / btc.volume)
-		#print(btc)
-    except BinanceAPIException as e:
-        print('CCXT Error')
-        print(e.status_code)
-        print(e.message)
-        print(e.code)
+    if PAIR_WITH != "BTC":
+        # Normal Scan for LIMIT and INTERVAL
+        exchange = ccxt.binance()
+        try:
+            btc = exchange.fetch_ohlcv("BTC" + PAIR_WITH, timeframe='1h', limit=25)
+            btc = pd.DataFrame(btc, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
+            btc['VWAP'] = ((((btc.high + btc.low + btc.close) / 3) * btc.volume) / btc.volume)
+            #print(btc)
+        except BinanceAPIException as e:
+            print('CCXT Error')
+            print(e.status_code)
+            print(e.message)
+            print(e.code)
 
-    try:
-        btc2 = btc.ta.sma(length=2)
-        btc3 = btc.ta.sma(length=3)
-        btc4 = btc.ta.sma(length=4)
-        btc5 = btc.ta.sma(length=5)
-        btc10 = btc.ta.sma(length=10)
-        btc20 = btc.ta.sma(length=20)
-        btc200 = btc.ta.sma(length=200)
-        
-        btc2 = btc2.iloc[-1]
-        btc3 = btc3.iloc[-1]
-        btc4 = btc4.iloc[-1]
-        btc5 = btc5.iloc[-1]
-        btc10 = btc10.iloc[-1]
-        btc20 = btc20.iloc[-1]
-        btc200 = btc200.iloc[-1]
-        
-        print(f'{btc2:.2f} {btc3:.2f} {btc4:.2f} {btc5:.2f} {btc10:.2f} {btc20:.2f} {btc200[2]:.2f}')
+        try:
+            btc2 = btc.ta.sma(length=2)
+            btc3 = btc.ta.sma(length=3)
+            btc4 = btc.ta.sma(length=4)
+            btc5 = btc.ta.sma(length=5)
+            btc10 = btc.ta.sma(length=10)
+            btc20 = btc.ta.sma(length=20)
+            btc200 = btc.ta.sma(length=200)
+            
+            btc2 = btc2.iloc[-1]
+            btc3 = btc3.iloc[-1]
+            btc4 = btc4.iloc[-1]
+            btc5 = btc5.iloc[-1]
+            btc10 = btc10.iloc[-1]
+            btc20 = btc20.iloc[-1]
+            btc200 = btc200.iloc[-1]
+            
+            print(f'{SIGNAL_NAME}: {txcolors.BUY}{btc2:.2f} {btc3:.2f} {btc4:.2f} {btc5:.2f} {btc10:.2f} {btc20:.2f} {btc200[2]:.2f}{txcolors.DEFAULT}')
 
-        paused = False
-        if btc2 > btc3 > btc4 > btc5 > btc10 > btc20 > btc200:
             paused = False
-            print(f'{SIGNAL_NAME}: Market looks OK')
+            if btc2 > btc3 > btc4 > btc5 > btc10 > btc20 > btc200:
+                paused = False
+                print(f'{SIGNAL_NAME}: {txcolors.BUY}Market looks OK{txcolors.DEFAULT}')
 
-        else:
-            print(f'{SIGNAL_NAME}: Market not looking good')
-            paused = True
-    except Exception as e:
-        print(f'{SIGNAL_NAME}: Exception analyse_btc() 1: {e}')
-        print("Error on line {}".format(sys.exc_info()[-1].tb_lineno))
-        pass
-    except KeyboardInterrupt as ki:
-        pass
+            else:
+                print(f'{SIGNAL_NAME}: Market not looking good{txcolors.DEFAULT}')
+                paused = True
+        except Exception as e:
+            print(f'{SIGNAL_NAME}: Exception analyse_btc() 1: {e}{txcolors.DEFAULT}')
+            print("Error on line {}".format(sys.exc_info()[-1].tb_lineno))
+            pass
+        except KeyboardInterrupt as ki:
+            pass
     return paused
 
 def do_work():
     global paused
     while True:
         try:
-            paused = analyse_btc()
-            if paused:
-                with open(f'signals/{SIGNAL_NAME}.{SIGNAL_TYPE}', 'a+') as f:
-                    f.write('yes')
-                print(f"paused by BTC")
+            if PAIR_WITH != "BTC":
+                paused = analyse_btc()
+                if paused:
+                    with open(f'signals/{SIGNAL_NAME}.{SIGNAL_TYPE}', 'a+') as f:
+                        f.write('yes')
+                    print(f'{SIGNAL_NAME}: {txcolors.BUY}Paused by BTC{txcolors.DEFAULT}')
 
+                else:
+                    if os.path.isfile(f'signals/{SIGNAL_NAME}.{SIGNAL_TYPE}'):
+                        os.remove(f'signals/{SIGNAL_NAME}.{SIGNAL_TYPE}')
+                    print(f'{SIGNAL_NAME}: {txcolors.BUY}Running...{txcolors.DEFAULT}')
+                time.sleep(30)
             else:
-                if os.path.isfile(f'signals/{SIGNAL_NAME}.{SIGNAL_TYPE}'):
-                    os.remove(f'signals/{SIGNAL_NAME}.{SIGNAL_TYPE}')
-                print(f"Running")
-            time.sleep(30)
+                print(f'{SIGNAL_NAME}: {txcolors.SELL_LOSS}This module cannot run under its own currency as a base, closing the module......{txcolors.DEFAULT}')
         except Exception as e:
-            print(f'{SIGNAL_NAME}: Exception do_work() 1: {e}')
+            print(f'{SIGNAL_NAME}: Exception do_work() 1: {e}{txcolors.DEFAULT}')
             print("Error on line {}".format(sys.exc_info()[-1].tb_lineno))
             continue
         except KeyboardInterrupt as ki:
